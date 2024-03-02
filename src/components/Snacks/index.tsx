d3336a6964e9ee783bdd0af4dba7b421190ac8d0
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-03-01 22:36:54 -0300

    feat: adding products to the feed :white_check_mark:

--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -1,6 +1,7 @@
 import { FiPlus } from 'react-icons/fi'
 
 import { SnackItemType } from '../../interface/SnackItemType'
+import { priceFormatter } from '../../utils/priceFormatter'
 import * as C from './styles'
 
 interface SnacksProps {
@@ -16,7 +17,7 @@ export const Snacks = ({ snacks }: SnacksProps) => {
                     <img src={snackItem.image} alt={snackItem.name} />
                     <p>{snackItem.description}</p>
                     <div>
-                        <strong>{snackItem.price}</strong>
+                        <strong>{priceFormatter.format(snackItem.price)}</strong>
                         <button type='button'>
                             <FiPlus />
                         </button>
