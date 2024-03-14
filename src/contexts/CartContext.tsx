a8d1ebc9a842e2c626b105845dfa67dc5409e7a4
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-03-14 20:23:03 -0300

    feat: update

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -19,7 +19,7 @@ interface UpdateCartProps {
 }
 
 interface CartContextProps {
-    cart: SnackItemType[]
+    cart: Snack[]
     addSnackIntoCart: (snack: SnackItemType) => void,
     // removeSnackFromCart: ({ id, snack }: RemoveSnackFromCart) => void,
     // updateCart: ({ id, snack, newQuantity }: UpdateCartProps) => void
