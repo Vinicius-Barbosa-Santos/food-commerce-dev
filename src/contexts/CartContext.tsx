b45772771c55d3b3f61fa12f86e48cbbe2937c63
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-03-14 09:03:35 -0300

    feat: adding the first cart functions :sparkles:

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -21,8 +21,8 @@ interface UpdateCartProps {
 interface CartContextProps {
     cart: SnackItemType[]
     addSnackIntoCart: (snack: SnackItemType) => void,
-    removeSnackFromCart: ({ id, snack }: RemoveSnackFromCart) => void,
-    updateCart: ({ id, snack, newQuantity }: UpdateCartProps) => void
+    // removeSnackFromCart: ({ id, snack }: RemoveSnackFromCart) => void,
+    // updateCart: ({ id, snack, newQuantity }: UpdateCartProps) => void
 }
 
 export const CartContext = createContext({} as CartContextProps)
@@ -32,8 +32,16 @@ interface CartProviderProps {
 }
 
 export const CartProvider = ({ children }: CartProviderProps) => {
+    const [cart, setCart] = useState<SnackItemType[]>([])
+
+    const addSnackIntoCart = (snack: SnackItemType): void => {
+        const newSnack = { ...snack, quantity: 1, subtotal: snack.price }
+        const newCart = [...cart, newSnack]
+        setCart(newCart)
+    }
+
     return (
-        <CartContext.Provider value={{}}>
+        <CartContext.Provider value={{ cart, addSnackIntoCart }}>
             {children}
         </CartContext.Provider>
     )
