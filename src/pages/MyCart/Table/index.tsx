e666a99ad22a8fd879bacabf7a48f03dcca8efa1
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-03-20 15:22:12 -0300

    feat: adjusting empty cart :sparkles:

--- src/pages/MyCart/Table/index.tsx
+++ src/pages/MyCart/Table/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 
+import { EmptyCart } from "../../../components/EmptyCart"
 import { useCart } from "../../../contexts/CartContext"
 import { TableDesktop } from './TableDesktop'
 import { TableMobile } from "./TableMobile"
@@ -23,7 +24,7 @@ export const Table = () => {
     }, [])
 
     if (cart.length === 0) {
-        return <h1>Ops! Parece que você não tem pedidos, peça já!</h1>
+        return <EmptyCart title="Ops! Parece que você não tem pedidos, peça já!" />
     }
 
     return windowWidth > 768 ? <TableDesktop /> : <TableMobile />
