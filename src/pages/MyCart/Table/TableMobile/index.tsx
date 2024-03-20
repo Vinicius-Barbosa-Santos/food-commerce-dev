379aa96f88071ee6fcdafe06b112719101dae39a
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-03-20 13:57:19 -0300

    feat: adjusting the cart for mobile :sparkles:

--- src/pages/MyCart/Table/TableMobile/index.tsx
+++ src/pages/MyCart/Table/TableMobile/index.tsx
@@ -1,7 +1,48 @@
+import { FaTrashAlt } from 'react-icons/fa'
+
+import minusImg from '../../../../assets/circle-minus.svg'
+import plusImg from '../../../../assets/circle-plus.svg'
+import { ConfirmOrder } from '../../../../components/ConfirmOrder'
+import { useCart } from '../../../../contexts/CartContext'
+import { priceFormatter } from '../../../../utils/priceFormatter'
+import * as C from './styles'
+
 export const TableMobile = () => {
-    return(
-        <div>
-            <h1>Table Mobile</h1>
-        </div>
+
+    const { cart, removeSnackFromCart, snackCartIncrement, snackCartDecrement } = useCart()
+
+    return (
+        <C.Container>
+            {cart.map((item) => (
+                <div key={`${item.snack}-${item.id}`} className='order-item'>
+                    <div>
+                        <img src={item.image} alt={item.name} />
+                    </div>
+                    <div>
+                        <h4>{item.name}</h4>
+                        <span>{priceFormatter.format(item.price)}</span>
+                        <div>
+                            <div>
+                                <button type='button' onClick={() => snackCartDecrement(item)}>
+                                    <img src={minusImg} alt='Remover quantidade' />
+                                </button>
+                                <span>{`${item.quantity}`.padStart(2, '0')}</span>
+                                <button type='button' onClick={() => snackCartIncrement(item)}>
+                                    <img src={plusImg} alt='Adicionar quantidade' />
+                                </button>
+                            </div>
+                            <button type='button' onClick={() => removeSnackFromCart(item)}>
+                                <FaTrashAlt />
+                            </button>
+                        </div>
+                        <h5>
+                            <span>Subtotal</span> {priceFormatter.format(item.subtotal)}
+                        </h5>
+                    </div>
+                </div>
+            ))}
+
+            <ConfirmOrder />
+        </C.Container>
     )
 }
