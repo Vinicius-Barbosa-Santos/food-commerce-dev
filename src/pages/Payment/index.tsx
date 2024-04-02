f4336b5e76a31f0b4727be655b7d4091d98edd09
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-04-02 11:59:20 -0300

    feat: creating the foundations for the form :sparkles:

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -7,7 +7,33 @@ export const Payment = () => {
         <C.Container>
             <Head title='Pagamento' />
             <OrderHeader />
-            <C.Inner>Formulário</C.Inner>
+            <C.Inner>
+                <C.Form>
+                    <h4>informações</h4>
+
+                    <div className='field'>
+                        <label htmlFor='full-name'>Nome e sobrenome</label>
+                        <input type='text' id='full-name' name='full-name' autoComplete='name' />
+                    </div>
+
+                    <div className='grouped'>
+                        <div className='field'>
+                            <label htmlFor='email'>E-mail</label>
+                            <input type='email' name='email' id='email' autoComplete='email' />
+                        </div>
+
+                        <div className='field'>
+                            <label htmlFor='mobile'>Celular</label>
+                            <input type='tel' id='mobile' name='mobile' autoComplete='phone' />
+                        </div>
+
+                        <div className='field'>
+                            <label htmlFor='document'>CPF / CNPJ</label>
+                            <input type='text' id='document' name='document' />
+                        </div>
+                    </div>
+                </C.Form>
+            </C.Inner>
         </C.Container>
     )
 }
