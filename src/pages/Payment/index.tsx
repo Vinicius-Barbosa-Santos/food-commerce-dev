4746131941c83c4bba0e59423cdd39af9adbf757
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-04-04 18:06:32 -0300

    feat: mobile validation :sparkles:

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,25 +1,12 @@
 import { yupResolver } from '@hookform/resolvers/yup'
 import { Controller, SubmitHandler, useForm } from 'react-hook-form'
 import { IMaskInput } from 'react-imask'
-import * as yup from 'yup'
 
 import { Head } from '../../components/Head'
 import { PayOrder } from '../../components/OrderCloseAction/PayOrder'
 import { OrderHeader } from '../../components/OrderHeader'
 import { Container, Form, Inner } from './styles'
-
-const schema = yup
-  .object({
-    fullName: yup
-      .string()
-      .required('Nome e sobrenome são obrigatórios.')
-      .min(3, 'Nome e sobrenome muito curto.'),
-    email: yup.string().email().required(),
-    mobile: yup.string().required(),
-  })
-  .required()
-
-type FieldValues = yup.InferType<typeof schema>
+import { FieldValues, schema } from './validationSchema'
 
 export const Payment = () => {
   const {
