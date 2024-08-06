fa8f2fbf58db15191c7d29cacf83f1c872da7cb5
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-08-06 15:19:05 -0300

    feat: cleaning imports :sparkles:

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from 'react-toastify'
 
