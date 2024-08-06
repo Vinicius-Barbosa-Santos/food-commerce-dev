f37f4051cf13438cf73d4d095f4b1e1c9d6f33ab
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-08-06 14:25:56 -0300

    feat: payment method :sparkles:

--- src/components/OrderCloseAction/PayOrder/index.tsx
+++ src/components/OrderCloseAction/PayOrder/index.tsx
@@ -1,3 +1,6 @@
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+
 import { useCart } from '../../../contexts/CartContext'
 import { priceFormatter } from '../../../utils/priceFormatter'
 import { Container } from '../style'
@@ -6,10 +9,16 @@ export const PayOrder = () => {
   const { cart } = useCart()
 
   const totalAmount = cart.reduce((acc, item) => (acc += item.subtotal), 0)
+  const navigate = useNavigate()
+
+  const handlePayment = () => {
+    toast.success(`Pagamento efetuado com sucesso!`)
+    navigate('/')
+  }
 
   return (
     <Container>
-      <button type='submit'>Pagar</button>
+      <button type='submit' onClick={handlePayment}>Pagar</button>
       <span>
         Total <strong>{priceFormatter.format(totalAmount)}</strong>
       </span>
